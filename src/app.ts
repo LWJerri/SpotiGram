5c0046f507f4cc3e565d5cb8c499b0001a22b1d2
LWJerri <loginmanx@gmail.com>
2023-06-30 05:24:17 +0300

    Add `app.ts`

---
+++ src/app.ts
@@ -0,0 +1,73 @@
+import "dotenv/config";
+import { TelegramClient } from "telegram";
+import { NewMessage, NewMessageEvent } from "telegram/events";
+import { StringSession } from "telegram/sessions";
+import { DEVICE_MODEL, SPOTIFY_REGEXP, SPOTIFY_TRACK_REGEXP } from "./helpers/constants";
+import { spotifyManager } from "./helpers/spotifyManager";
+
+const { APP_ID, APP_HASH, SESSION } = process.env;
+
+const stringSession = new StringSession(SESSION);
+
+export const telegram = new TelegramClient(stringSession, parseInt(APP_ID), APP_HASH, { deviceModel: DEVICE_MODEL });
+
+async function startClient() {
+  await telegram.connect();
+
+  console.log("Connected to User API server!");
+
+  const meMessage = await telegram.sendMessage("me", { message: DEVICE_MODEL });
+  await meMessage.delete();
+
+  console.log("Now, script ready to listen all actions from connected client...");
+
+  telegram.addEventHandler(messageEvent, new NewMessage());
+
+  async function messageEvent(event: NewMessageEvent) {
+    if (!event.isPrivate) return;
+
+    const getCurrentUser = await telegram.getMe();
+    const clientId = getCurrentUser.CONSTRUCTOR_ID === 2409088552 && getCurrentUser.id.toJSNumber();
+
+    const { text: message } = event.message;
+
+    const messageClientId =
+      event.originalUpdate.CONSTRUCTOR_ID === 522914557 &&
+      event.originalUpdate.message.peerId.className === "PeerUser" &&
+      event.originalUpdate.message.peerId.userId.toJSNumber();
+
+    if (clientId !== messageClientId && event.message.entities?.length) {
+      const spotifyParsedIds =
+        event.message.entities
+          .filter((entity) => {
+            const isSpotifyEntity =
+              entity.className === "MessageEntityTextUrl" && SPOTIFY_TRACK_REGEXP.test(entity.url);
+
+            return entity.className === "MessageEntityUrl" || isSpotifyEntity;
+          })
+          .filter((entity) => {
+            if (entity.className === "MessageEntityUrl") {
+              return SPOTIFY_REGEXP.test(message.slice(entity.offset, entity.offset + entity.length));
+            } else {
+              return entity;
+            }
+          })
+          .map((entity) => {
+            if (entity.className === "MessageEntityTextUrl") {
+              return entity.url.replace(SPOTIFY_TRACK_REGEXP, "");
+            } else {
+              const [spotifyURL] = message.slice(entity.offset, entity.offset + entity.length).match(SPOTIFY_REGEXP);
+              const spotifySongId = spotifyURL.replace(SPOTIFY_TRACK_REGEXP, "");
+
+              return spotifySongId;
+            }
+          }) ?? [];
+
+      if (spotifyParsedIds.length > 0) {
+        return await spotifyManager(event, [...new Set(spotifyParsedIds)]);
+      }
+    }
+  }
+}
+
+startClient();
