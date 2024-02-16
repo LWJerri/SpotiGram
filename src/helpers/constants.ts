38b145ecabb9d2758d2f36538094141dafa1adf9
LWJerri <loginmanx@gmail.com>
2024-02-16 07:46:32 +0100

    Remove unused constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,7 +1,5 @@
-const SPOTIFY_API_URL = "https://api.spotify.com/v1";
-const DEVICE_MODEL = "User API";
+const SPOTIFY_URL_REGEXP = /https?:\/\/open.spotify.com\/(track)\/[a-zA-Z0-9]+/;
+const SPOTIFY_TRACK_ID_REGEXP = /[^/]+$/;
+const ODESLI_BOT_ID = 899433974;
 
-const SPOTIFY_REGEXP = /https?:\/\/open\.spotify\.com\/track\/[a-zA-Z0-9]+/;
-const SPOTIFY_TRACK_REGEXP = /https?:\/\/open\.spotify\.com\/track\//;
-
-export { DEVICE_MODEL, SPOTIFY_API_URL, SPOTIFY_REGEXP, SPOTIFY_TRACK_REGEXP };
+export { ODESLI_BOT_ID, SPOTIFY_TRACK_ID_REGEXP, SPOTIFY_URL_REGEXP };
