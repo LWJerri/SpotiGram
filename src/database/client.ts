cdfc54314b7a2026ed2c283673809369b1419866
LWJerri <loginmanx@gmail.com>
2024-02-17 05:02:33 +0100

    Disable any `NOTICE` messages in console

--- src/database/client.ts
+++ src/database/client.ts
@@ -3,6 +3,6 @@ import postgres from "postgres";
 import { env } from "../helpers/env";
 import * as schema from "./schema";
 
-const client = postgres(env.SUPABASE_DB_URL);
+const client = postgres(env.SUPABASE_DB_URL, { onnotice() {} });
 
 export const orm = drizzle(client, { schema });
