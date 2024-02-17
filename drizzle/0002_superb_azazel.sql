936101df113604139279bed3a6fa3facbc036fe8
LWJerri <loginmanx@gmail.com>
2024-02-17 05:22:05 +0100

    Add `notNull()` params to fields

---
+++ drizzle/0002_superb_azazel.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "playlists" ALTER COLUMN "snapshotId" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "tracks" ALTER COLUMN "addedAt" SET NOT NULL;
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -2,10 +2,10 @@ import { date, pgTable, text } from "drizzle-orm/pg-core";
 
 export const playlists = pgTable("playlists", {
   id: text("id").primaryKey(),
-  snapshotId: text("snapshotId"),
+  snapshotId: text("snapshotId").notNull(),
 });
 
 export const tracks = pgTable("tracks", {
   id: text("id").primaryKey(),
-  addedAt: date("addedAt"),
+  addedAt: date("addedAt").notNull(),
 });
