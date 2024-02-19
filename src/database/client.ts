1bf5d310290b8154d08149c20aeb1574b07f6616
LWJerri <loginmanx@gmail.com>
2024-02-19 13:15:53 +0100

    Remove `onnitoce`

--- src/database/client.ts
+++ src/database/client.ts
@@ -3,6 +3,6 @@ import postgres from "postgres";
 import { env } from "../helpers/env";
 import * as schema from "./schema";
 
-const client = postgres(env.SUPABASE_DB_URL, { onnotice() {} });
+const client = postgres(env.SUPABASE_DB_URL);
 
 export const orm = drizzle(client, { schema });
