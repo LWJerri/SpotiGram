a40104e092d0480a8013b923fdd2ab529f818927
LWJerri <loginmanx@gmail.com>
2024-02-22 18:29:34 +0100

    Code optimization

--- src/filters/isViaOdesliBot.ts
+++ src/filters/isViaOdesliBot.ts
@@ -2,4 +2,4 @@ import { UpdateFilter } from "@mtcute/dispatcher";
 import { Message } from "@mtcute/node";
 import { ODESLI_BOT_ID } from "../helpers/constants";
 
-export const isViaOdesliBot: UpdateFilter<Message> = (msg) => Boolean(msg.viaBot && msg.viaBot.id === ODESLI_BOT_ID);
+export const isViaOdesliBot: UpdateFilter<Message> = (msg) => !!msg.viaBot && msg.viaBot.id === ODESLI_BOT_ID;
