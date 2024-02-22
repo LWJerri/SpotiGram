ce06539eefd80b4c0f8a1433ab97089c4cd73dae
LWJerri <loginmanx@gmail.com>
2024-02-22 21:44:55 +0100

    Delete validators

--- src/validators/alphanum.ts
+++
@@ -1,7 +0,0 @@
-import { makeValidator } from "envalid";
-
-export const alphanum = makeValidator((value) => {
-  if (typeof value !== "string" || !value.length) throw new Error("Value must be a non-empty string");
-
-  return value;
-});
