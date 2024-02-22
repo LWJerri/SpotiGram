72c95f88903b14f6c391c8aa79e133e666e0c4c8
LWJerri <loginmanx@gmail.com>
2024-02-22 11:54:56 +0100

    Add `node-cron`, move dispatchers code into specific files

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,28 @@
 import { UpdateState, filters } from "@mtcute/dispatcher";
 import "dotenv/config";
-import { isHaveUrlEntities, isSpotifyUrl, isViaOdesliBot } from "./filters";
-import { ODESLI_BOT_ID, SPOTIFY_TRACK_ID_REGEXP } from "./helpers/constants";
+import { schedule } from "node-cron";
+import { odesli, urlLinks } from "./dispatchers";
+import { isHaveUrlEntities, isViaOdesliBot } from "./filters";
 import { env } from "./helpers/env";
-import inlineCall from "./helpers/inlineCall";
 import saveSession from "./helpers/saveSession";
 import { SpotifyManager } from "./manager";
 import { client, dispatcher } from "./mtcute";
+import { processQueue } from "./schedulers";
 
 const spotifyManager = new SpotifyManager();
 
-dispatcher.onNewMessage(filters.and(filters.chat("private"), filters.not(filters.me), isViaOdesliBot), async (msg) => {
-  const spotifyUrlEntity = msg.entities.find(({ params }) => params.kind === "text_link" && isSpotifyUrl(params.url));
-
-  if (!spotifyUrlEntity || !spotifyUrlEntity.is("text_link")) return;
-
-  const [trackId] = spotifyUrlEntity.params.url.match(SPOTIFY_TRACK_ID_REGEXP)!;
-
-  await spotifyManager.process([trackId], msg.chat.id);
-});
+dispatcher.onNewMessage(
+  filters.and(filters.chat("private"), filters.not(filters.me), isViaOdesliBot),
+  async (msg) => await odesli(msg),
+);
 
 dispatcher.onNewMessage(
   filters.and(filters.chat("private"), filters.not(filters.me), isHaveUrlEntities),
-  async (msg, state: UpdateState<{}>) => {
-    let trackListIds = [];
-
-    for (let i = 0; i < msg.urlEntities.length; i++) {
-      await state.throttle("inlineCallsCycle", 5, 10);
-
-      const inlineCallResult = await inlineCall(msg.urlEntities[i].text, ODESLI_BOT_ID);
-      const inlineResult = inlineCallResult?.results.pop();
-
-      if (inlineResult?.sendMessage._ !== "botInlineMessageText" || !inlineResult.sendMessage.entities?.length) {
-        continue;
-      }
-
-      const spotifyEntity = inlineResult.sendMessage.entities.find(
-        (entity) => entity._ === "messageEntityTextUrl" && isSpotifyUrl(entity.url),
-      );
-
-      if (spotifyEntity?._ !== "messageEntityTextUrl") continue;
-
-      const [trackId] = spotifyEntity.url.match(SPOTIFY_TRACK_ID_REGEXP)!;
-
-      trackListIds.push(trackId);
-    }
-
-    if (!trackListIds.length) return;
-
-    await spotifyManager.process([...new Set(trackListIds)], msg.chat.id);
-  },
+  async (msg, state: UpdateState<{}>) => await urlLinks(msg, state),
 );
+
+schedule("0 * * * *", async () => await spotifyManager.synchronize());
+schedule("*/5 * * * *", async () => await processQueue(spotifyManager));
 
 client.run({ session: env.TG_SESSION }, async () => {
   if (env.SAVE_SESSION) return await saveSession();
