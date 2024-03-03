14ee86438270fb8bf7b107b3081f2345a391e94a
LWJerri <loginmanx@gmail.com>
2024-03-03 14:14:44 +0100

    Set name & version for session

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,4 +1,8 @@
+import { readFile } from "fs/promises";
+import { PackageJson } from "type-fest";
+
 const SPOTIFY_TRACK_ID_REGEXP = /[^/]+$/;
 const ODESLI_BOT_ID = 899433974;
+const packageJson: PackageJson = JSON.parse(await readFile("package.json", { encoding: "utf8" }));
 
-export { ODESLI_BOT_ID, SPOTIFY_TRACK_ID_REGEXP };
+export { ODESLI_BOT_ID, SPOTIFY_TRACK_ID_REGEXP, packageJson };
--- src/telegram/client.ts
+++ src/telegram/client.ts
@@ -1,8 +1,13 @@
 import { NodeTelegramClient } from "@mtcute/node";
 import { environment } from "../config/environment.js";
+import { packageJson } from "../helpers/constants.js";
 
 export const client = new NodeTelegramClient({
   apiId: environment.TG_API_ID,
   apiHash: environment.TG_API_HASH,
   storage: "spotigram.session",
+  initConnectionOptions: {
+    appVersion: packageJson.version,
+    deviceModel: packageJson.name,
+  },
 });
